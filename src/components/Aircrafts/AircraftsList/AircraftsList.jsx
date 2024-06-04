f8277e48f4bca8b7180abb268eb172788485d417
Diego Cerezo <diego.cerezo@gmail.com>
2024-06-04 20:20:54 +0200

    aircraft list reviewed

--- src/components/Aircrafts/AircraftsList/AircraftsList.jsx
+++ src/components/Aircrafts/AircraftsList/AircraftsList.jsx
@@ -1,19 +1,17 @@
-import "./AircraftsList"
 import { Container } from "react-bootstrap";
 import AircraftCard from "../AircraftCard/AircraftCard";
 
-
-
-const AircraftsList = () => {
+const AircraftsList = ({ aircraft }) => {
     return (
-        <>
-            <Container className="mt-5 pt-5 pb-5">
+        <Container className="mt-5 pt-5 pb-5">
+            <AircraftCard
+                _id={aircraft._id}
+                mainImage={aircraft.mainImage}
+                manufacturer={aircraft.manufacturer}
+                manufacturerDescription={aircraft.manufacturerDescription}
 
-                <AircraftCard />
-
-            </Container>
-        </>
-
+            />
+        </Container>
     );
 };
 
