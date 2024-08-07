252d7382db617e6b743927eff64f96d25ab5dae1
Diego Cerezo <diego.cerezo@gmail.com>
2024-08-07 17:31:47 +0200

    conflict solved

--- src/services/profile.services.js
+++ src/services/profile.services.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-
-class ProfileServices {
-
-    constructor() {
-
-        this.axiosApp = axios.create({
-            baseURL: `${import.meta.env.VITE_API_URL}/profile`
-        })
-
-        this.axiosApp.interceptors.request.use(config => {
-
-            const storedToken = localStorage.getItem('authToken');
-
-            if (storedToken) {
-                config.headers = { Authorization: `Bearer ${storedToken}` };
-            }
-
-            return config
-        })
-    }
-
-    getAllProfiles() {
-        return this.axiosApp.get('/')
-    }
-
-    getOneProfile(profileId) {
-        return this.axiosApp.get(`/${profileId}`)
-    }
-
-    saveProfile(profileData) {
-        return this.axiosApp.post('/', profileData)
-    }
-
-    editProfile(profileId, profileData) {
-        return this.axiosApp.put(`/${profileId}`, profileData)
-    }
-
-    getProfileByOwner(ownerId) {
-        return this.axiosApp.get(`/owner/${ownerId}`)
-    }
-
-}
-
-const profileServices = new ProfileServices()
-
-export default profileServices
-=======
 import axios from 'axios';
 
 class ProfileServices {
@@ -87,4 +38,3 @@ class ProfileServices {
 
 const profileServices = new ProfileServices();
 export default profileServices;
->>>>>>> diego
